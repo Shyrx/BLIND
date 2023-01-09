1be13ad62387d3f02b559fb4bc15f0bd5bdb6970
Antoine Solé <antoine.sole@epita.fr>
2023-01-09 13:43:58 +0100

    feat(flake): first version

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Development environment for the BLIND project";
+
+  inputs = { nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          gcc
+          gnumake
+          clang-tools
+          gdb
+          bear
+          cmake
+          cmake-language-server
+          opencv
+          arduino
+          python310
+        ];
+      };
+    };
+}
