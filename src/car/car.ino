b1bea07d8d3ea849ea134b87dc4744399de9bf30
Antoine Solé <antoine.sole@epita.fr>
2023-01-15 17:05:24 +0100

    feat(arduino): use code from tele

--- src/car/car.ino
+++ src/car/car.ino
@@ -1,20 +1,54 @@
+#include <Servo.h>
+
+int PIN = 13;
+
+int front = 92;
+int dir = 7;
+int spd = 6;
+int forward = LOW;
+int backward = HIGH;
+int minSpd = 0;
+int maxSpd = 255;
+
 int receivedByte;
+
+void moveForward(int rotation)
+{
+    digitalWrite(dir, forward);
+    analogWrite(spd, rotation);
+}
+
+void stop()
+{
+    analogWrite(spd, minSpd);
+}
+
+void turn(int degree)
+{
+    if (degree < 72)
+        degree = 72;
+    if (degree > 108)
+        degree = 108;
+
+    servo.write(degree);
+}
 
 void handleInput(char input) {
     Serial.write(input);
 }
 
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, LOW);
+  servo.attach(PIN);
+  pinMode(PIN, OUTPUT);
+  pinMode(dir, OUTPUT);
+  pinMode(spd, OUTPUT);
+  resetDir();
 
   Serial.begin(115200);
   while (!Serial) {
     ;
   }
-
-  // turn on LED when ready to communicate
-  digitalWrite(LED_BUILTIN, HIGH);
+  delay(300);
 }
 
 void loop() {
